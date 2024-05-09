b4bda5073497843d9cccbbf8dfbec256a78c1956
aniket <aniket.umbc@gmail.com>
2024-05-09 14:41:16 +0530

    Global colors file added

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { LinearGradient } from 'expo-linear-gradient';
 import { useState } from 'react';
 import GameScreen from './screens/GameScreen';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import Colors from './constant/colors';
 
 export default function App() {
   const [userNumber, setUserNumber] = useState(null);
@@ -19,7 +20,10 @@ export default function App() {
   }
 
   return (
-    <LinearGradient colors={['#4e0329', '#ddb52f']} style={styles.rootScreen}>
+    <LinearGradient
+      colors={[Colors.primary700, Colors.accent500]}
+      style={styles.rootScreen}
+    >
       <ImageBackground
         source={require('./assets/images/background.png')}
         resizeMode='cover'
--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { TextInput, View, StyleSheet, Alert } from 'react-native';
 import PrimaryButton from '../components/PrimaryButton';
+import Colors from '../constant/colors';
 
 function StartGameScreen({ pickNumber }) {
   const [enterNumber, setEnteredNumber] = useState('');
@@ -61,7 +62,7 @@ const styles = StyleSheet.create({
     marginTop: 100,
     padding: 16,
     marginHorizontal: 24,
-    backgroundColor: '#3b021f',
+    backgroundColor: Colors.primary800,
     borderRadius: 8,
 
     elevation: 4,
@@ -74,9 +75,9 @@ const styles = StyleSheet.create({
     height: 50,
     fontSize: 32,
     width: 50,
-    borderBottomColor: '#ddb52f',
+    borderBottomColor: Colors.accent500,
     borderBottomWidth: 2,
-    color: '#ddb52f',
+    color: Colors.accent500,
     marginVertical: 8,
     fontWeight: 'bold',
     textAlign: 'center',
